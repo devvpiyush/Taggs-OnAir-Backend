5e9d506204cc63fbd1321c868e24a424e78cf2b0
Piyush <gsssmirzewala1954@gmail.com>
2026-03-20 12:43:27 +0530

    NEW UPDATE 12:43 PM (20th Mar. 2026)
    Created "Post" Model.

---
+++ models/post.model.js
@@ -0,0 +1,24 @@
+// External Modules
+import mongoose from "mongoose";
+
+const PostSchema = mongoose.Schema(
+  {
+    type: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      enum: ["thread", "post"],
+      default: "thread",
+    },
+    postedBy: { type: mongoose.Types.ObjectId, required: true },
+    caption: { type: String, trim: true, default: "", maxlength: 850 },
+    viewsCount: { type: Number, default: 0 },
+    likesCount: { type: Number, default: 0 },
+    commentsCount: { type: Number, default: 0 },
+    reportsCount: { type: Number, default: 0 },
+    isDeleted: { type: Boolean, default: false },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Post", PostSchema);
